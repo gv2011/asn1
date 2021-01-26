b3ab0c0e15159bc94717ad4e0ad22f44d17b73de
Vinz <gv2011@users.noreply.github.com>
2021-01-26 13:29:19 +0100

    moved out mail

--- tls/src/main/jdk1.4/org/bouncycastle/tls/crypto/impl/bc/Exceptions.java
+++
@@ -1,9 +0,0 @@
-package org.bouncycastle.tls.crypto.impl.bc;
-
-class Exceptions
-{
-    static IllegalArgumentException illegalArgumentException(String message, Throwable cause)
-    {
-        return new org.bouncycastle.tls.exception.IllegalArgumentException(message, cause);
-    }
-}
